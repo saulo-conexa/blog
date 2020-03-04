799c51c857416c7d7ff6b3fdaa6f21062960a87e
Saulo de Tarso <saulo.conexa@gmail.com>
2020-03-04 11:40:22 -0300

    Utilizando gerador de código GIIX

---
+++ protected/views/usuario/_form.php
@@ -0,0 +1,54 @@
+<?php
+/* @var $this UsuarioController */
+/* @var $model Usuario */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'usuario-form',
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'id'); ?>
+		<?php echo $form->textField($model,'id'); ?>
+		<?php echo $form->error($model,'id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'nome'); ?>
+		<?php echo $form->textField($model,'nome',array('size'=>60,'maxlength'=>100)); ?>
+		<?php echo $form->error($model,'nome'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'email'); ?>
+		<?php echo $form->textField($model,'email',array('size'=>60,'maxlength'=>100)); ?>
+		<?php echo $form->error($model,'email'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'senha'); ?>
+		<?php echo $form->textField($model,'senha',array('size'=>60,'maxlength'=>100)); ?>
+		<?php echo $form->error($model,'senha'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'biografia'); ?>
+		<?php echo $form->textField($model,'biografia',array('size'=>60,'maxlength'=>255)); ?>
+		<?php echo $form->error($model,'biografia'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
---
+++ protected/views/usuario/_view.php
@@ -0,0 +1,29 @@
+<?php
+/* @var $this UsuarioController */
+/* @var $data Usuario */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('nome')); ?>:</b>
+	<?php echo CHtml::encode($data->nome); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('email')); ?>:</b>
+	<?php echo CHtml::encode($data->email); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('senha')); ?>:</b>
+	<?php echo CHtml::encode($data->senha); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('biografia')); ?>:</b>
+	<?php echo CHtml::encode($data->biografia); ?>
+	<br />
+
+
+</div>
